7d39ffcf4c523df1c630b9b5a9702f5724fa9e4a
Franco Jalil <francojalil7@gmail.com>
2022-03-17 21:11:38 -0300

    agregue funcion existeProducto

--- js/script.js
+++ js/script.js
@@ -15,7 +15,34 @@ class Producto{
 let entrada = prompt("desea cargar productos? ponga si o no")
 
 
+if(entrada.toUpperCase() == "SI"){
 
-for(let i = 0; i < cantP; i++){
-    productos.push(new Producto(prompt("ingrese tipo producto"), prompt("ingrese vencimiento de producto"), prompt("ingrese precio")))
+    cantP = prompt("cuantos productos desea agregar?")
+
+    if(tipeof(cantP) === 'number'){
+
+        for(let i = 0; i < cantP; i++){
+            productos.push(new Producto(prompt("ingrese tipo producto"), prompt("ingrese vencimiento de producto"), prompt("ingrese precio")))
+        }
+
+    }
+    
+}
+
+
+function calcularGanancias(productos){
+    let ganancias = 0
+    for(let i = 0; i < productos.length; i++){
+        ganancias += productos[i].precio
+    }
+    return ganancias
+}
+
+function existeProducto(productos, producto){
+    let existe = false
+    for(let i = 0; i < productos.length; i++){
+        if(productos[i] == producto){
+        existe = true
+    }
+    return existe
 }
